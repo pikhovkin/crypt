77cf8e59147243775d61776deb38385b7b875d7f
pikhovkin@gmail.com <C:\Documents and Settings\pikhovkin\Application Data\The Bat!>
2011-03-10 15:47:11 +0300

    second commit

---
+++ crypt_heidisql.py
@@ -0,0 +1,37 @@
+﻿# -*- coding: utf-8 -*-
+
+def encrypt(password):
+    from random import randrange
+    result = ''
+    salt = randrange(1, 10)
+    for c in password:
+        nr = ord(c) + salt
+        if nr > 255:
+            nr -= 255
+        h = hex(nr)[2:]
+        if len(h) == 1:
+            h = '0'.join(h)
+        result += h
+
+    return result + str(salt)
+
+def decrypt(password):
+    if not password:
+        return ''
+
+    try:
+        salt = int(password[-1])
+    except ValueError:
+        salt = 0
+
+    result = ''
+    i = 0
+    n = len(password) - 1
+    while i < n:
+        nr = int(float.fromhex(''.join(['0x', password[i:i + 2]]))) - salt
+        if nr < 0:
+            nr += 255
+        result += chr(nr);
+        i += 2
+
+    return result
---
+++ crypt_sqlyog.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+
+from base64 import encodestring, decodestring
+
+
+def RotateBitRight(password):
+    return ''.join(
+        [chr(((ord(c) >> 1) | (ord(c) << 7)) & 255) for c in password])
+
+def RotateBitLeft(password):
+    return ''.join(
+        [chr(((ord(c) << 1) | (ord(c) >> 7)) & 255) for c in password])
+
+def DecodePassword(password):
+    return RotateBitLeft(decodestring(password)).strip()
+
+def EncodePassword(password):
+    return encodestring(RotateBitRight(password)).strip()
